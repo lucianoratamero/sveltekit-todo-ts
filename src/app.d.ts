b2f0e45343c725d7d22bd85ba5811fbc5a703f6e
Luciano Ratamero <luciano@ratamero.com>
2022-10-31 20:44:01 -0300

    Add type to handle results of function executions

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,9 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+
+export type Result<T, U> = ["ok", T] | ["error", U];
+
 declare namespace App {
 	// interface Locals {}
 	// interface PageData {}
